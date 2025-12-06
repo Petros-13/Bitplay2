1cda6929e1aadf612fda411ba5d90fdf0b37853d
Petros-13 <petroskalkounos78@gmail.com>
2025-12-06 14:55:51 +0100

    2nd commit

---
+++ Main.2.py
@@ -0,0 +1,14 @@
+def oddOccuring(arr):
+    res = 0
+    for element in arr:
+        res = res ^ element
+    return res
+
+arr = []
+n = int(input("Enter array size: "))
+while n:
+    num = int(input("Enter number: "))
+    arr.append(num)
+    n -= 1
+
+print("Odd occurring number is", oddOccuring(arr))
